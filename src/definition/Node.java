a7d89c20b8f869255f2fc00bb518bf6a25a03f87
muskangarg16 <muskan.garg_cs18@gla.ac.in>
2020-02-24 18:51:34 +0530

    creating getter and setter of Node class

--- src/definition/Node.java
+++ src/definition/Node.java
@@ -8,4 +8,20 @@ public class Node {
         this.data = data;
         this.next = next;
     }
+
+    public Person getData() {
+        return data;
+    }
+
+    public void setData(Person data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
