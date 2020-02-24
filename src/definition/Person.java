f3305526c7a877d6d65127ffba793dac6b1f8e25
muskangarg16 <muskan.garg_cs18@gla.ac.in>
2020-02-24 18:55:12 +0530

    declaring getter and setter of Person class

--- src/definition/Person.java
+++ src/definition/Person.java
@@ -9,4 +9,35 @@ public class Person {
     private ArrayList<BigInteger> contactList = new ArrayList<>();
     private String email;
 
+    public String getFName() {
+        return FName;
+    }
+
+    public void setFName(String FName) {
+        this.FName = FName;
+    }
+
+    public String getLName() {
+        return LName;
+    }
+
+    public void setLName(String LName) {
+        this.LName = LName;
+    }
+
+    public ArrayList<BigInteger> getContactList() {
+        return contactList;
+    }
+
+    public void setContactList(ArrayList<BigInteger> contactList) {
+        this.contactList = contactList;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
