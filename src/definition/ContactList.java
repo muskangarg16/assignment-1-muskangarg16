cb25620e3f0bca499945f4f022af49af2a8daf33
muskangarg16 <muskan.garg_cs18@gla.ac.in>
2020-02-24 20:57:14 +0530

    declaring member fields of ContactListAdt

--- src/definition/ContactList.java
+++ src/definition/ContactList.java
@@ -3,6 +3,31 @@ package definition;
 import adt.MyContactListAdt;
 
 public class ContactList implements MyContactListAdt<Person> {
+    private Node head;
+    private int size;
+    public Node getHead(){
+        return head;
+    }
+    public ContactList(){
+        head=null;
+    }
+    @Override
+    public void add(Person data) {
 
+    }
 
+    @Override
+    public void remove(int item) {
+
+    }
+
+    @Override
+    public void search(String first) {
+
+    }
+
+    @Override
+    public void view() {
+
+    }
 }
