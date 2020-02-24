049c80ab53f782f7d37cc0d153678a4e51394a77
muskangarg16 <muskan.garg_cs18@gla.ac.in>
2020-02-24 18:57:26 +0530

    creating toString method of Person class

--- src/definition/Person.java
+++ src/definition/Person.java
@@ -40,4 +40,14 @@ public class Person {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "FName='" + FName + '\'' +
+                ", LName='" + LName + '\'' +
+                ", contactList=" + contactList +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
