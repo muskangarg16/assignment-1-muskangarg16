faf678555c236736248ffc11d741a13d4072a9e6
muskangarg16 <muskan.garg_cs18@gla.ac.in>
2020-02-24 21:04:22 +0530

    creating object of ContactList

--- src/execution/Main.java
+++ src/execution/Main.java
@@ -1,5 +1,17 @@
 package execution;
 
-public class Main {
 
+import definition.MaintainContactList;
+import definition.Person;
+import definition.ContactList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ContactList list = new ContactList();
+        MaintainContactList obj= new MaintainContactList();
+        Person per =new Person();
+
+    }
 }
