06d2dfb8bb0cf9f2606aa8f6466f777906e33d8d
muskangarg16 <muskan.garg_cs18@gla.ac.in>
2020-02-24 18:54:36 +0530

    declaring member fields of Person class

--- src/definition/Person.java
+++ src/definition/Person.java
@@ -1,4 +1,12 @@
 package definition;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+
 public class Person {
+    private String FName;
+    private String LName;
+    private ArrayList<BigInteger> contactList = new ArrayList<>();
+    private String email;
+
 }
