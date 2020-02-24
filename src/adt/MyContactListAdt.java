646ba06dad0bf1e49536956c57a6aa04459ade41
muskangarg16 <muskan.garg_cs18@gla.ac.in>
2020-02-24 20:21:11 +0530

    methods of ADT

--- src/adt/MyContactListAdt.java
+++ src/adt/MyContactListAdt.java
@@ -3,7 +3,13 @@ package adt;
 import definition.Person;
 
 public interface MyContactListAdt {
+    public void add(Person data);
 
+    public void remove(int item);
+
+    public void search(String first);
+
+    public void view();
 
 
 }
